24acc59619e8eb1527eed5ac270635d28645f797
Hedy <hedywoo@gmail.com>
2016-05-30 11:40:07 -0600

    Pass item controller with nothing rendering

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ItemsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#index' do
+    get :index
+
+    assert_response :success
+  end
 end
